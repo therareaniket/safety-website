619fa120228bb3747bfc08a189d76f0ed2e7df01
shalugandhi <shalakagandhi81@gmail.com>
2026-01-30 13:51:57 +0530

    plan section updated

--- src/components/Homepage/HomePlans.tsx
+++ src/components/Homepage/HomePlans.tsx
@@ -4,53 +4,55 @@ import Link from "next/link";
 export default function HomePlans() {
     return (
 
-        <section className="plans-wrapper section pt-0">
+        <section className="plans-wrapper bg-dark-grey section">
             <div className="container">
                 <div className="main-title center text-center">
-                    <h2 className="text-md">Plans Designed for Smarter Safety Management</h2>
+                    <h2 className="text-md text-white">Not Sure Which Plan Fits You?</h2>
+                    <p className="h6 text-white">Talk to our team and get a custom quote within 24 hours.</p>
                 </div>
                 <div className="plans-row flex">
-                    <div className="plan-item bg-dark-grey">
+                    <div className="plan-item">
                         <div className="plan-title flex">
                             <h3 className="h4 text-sb text-white">Standard</h3>
                             <div className="p-price flex"><h4 className="text-bd text-white">$49</h4><span className="text-18 text-white">per user/month</span></div>
                         </div>
                         <p className="h6 text-white">A streamlined plan with all the essential tools for efficient case trials and management.</p>
-                        <div className="plan-list">
+                         <div className="plan-btn">
+                                <button className='main-btn flex'><span className='btn-icon bg-white centered-block'><svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M0.209996 10.1677C-0.0420067 10.3829 -0.0718591 10.7616 0.143319 11.0136C0.358496 11.2656 0.73722 11.2955 0.989223 11.0803L0.599609 10.624L0.209996 10.1677ZM12.4407 1.07116C12.4667 0.740812 12.22 0.451905 11.8897 0.425866L6.50638 0.00153416C6.17604 -0.0245046 5.88713 0.222185 5.86109 0.552531C5.83505 0.882877 6.08174 1.17178 6.41209 1.19782L11.1972 1.57501L10.8201 6.36017C10.794 6.69051 11.0407 6.97942 11.3711 7.00546C11.7014 7.0315 11.9903 6.78481 12.0164 6.45446L12.4407 1.07116ZM0.599609 10.624L0.989223 11.0803L12.2322 1.4803L11.8425 1.02401L11.4529 0.56772L0.209996 10.1677L0.599609 10.624Z" fill="black" />
+                                </svg></span><span className='text-md text-white text-16'>Get Your Plans</span></button>
+                            </div>
+                        {/* <div className="plan-list">
                             <h5 className="text-md text-white">Includes</h5>
                             <ul>
                                 <li className="plan-list-text flex"><svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.2454 11.066L0 5.82055L1.31135 4.5092L5.2454 8.44325L13.6887 0L15 1.31135L5.2454 11.066Z" fill="white" /></svg><p className="text-18 text-white">ICSR Case Management</p></li>
                                 <li className="plan-list-text flex"><svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.2454 11.066L0 5.82055L1.31135 4.5092L5.2454 8.44325L13.6887 0L15 1.31135L5.2454 11.066Z" fill="white" /></svg><p className="text-18 text-white">Safety Data Intake from Multiple Sources</p></li>
                                 <li className="plan-list-text flex"><svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.2454 11.066L0 5.82055L1.31135 4.5092L5.2454 8.44325L13.6887 0L15 1.31135L5.2454 11.066Z" fill="white" /></svg><p className="text-18 text-white">Basic Signal Detection & Tracking</p></li>
                                 <li className="plan-list-text flex"><svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.2454 11.066L0 5.82055L1.31135 4.5092L5.2454 8.44325L13.6887 0L15 1.31135L5.2454 11.066Z" fill="white" /></svg><p className="text-18 text-white">Compliance Tracking & Audit Trails</p></li>
-                            </ul>
-                            <div className="plan-btn">
+                            </ul>                           
+                        </div> */}
+                    </div>
+                    <div className="plan-separator"></div>
+                    <div className="plan-item">
+                        <div className="plan-title flex">
+                            <h3 className="h4 text-sb text-white">Enterprise</h3>
+                            <div className="p-price flex"><h4 className="text-bd text-white">$299</h4><span className="text-18 text-white"> per user/month</span></div>
+                        </div>
+                        <p className="h6 text-white">A streamlined plan with all the essential tools for efficient case trials and management.</p>
+                        <div className="plan-btn">
                                 <button className='main-btn flex'><span className='btn-icon bg-white centered-block'><svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M0.209996 10.1677C-0.0420067 10.3829 -0.0718591 10.7616 0.143319 11.0136C0.358496 11.2656 0.73722 11.2955 0.989223 11.0803L0.599609 10.624L0.209996 10.1677ZM12.4407 1.07116C12.4667 0.740812 12.22 0.451905 11.8897 0.425866L6.50638 0.00153416C6.17604 -0.0245046 5.88713 0.222185 5.86109 0.552531C5.83505 0.882877 6.08174 1.17178 6.41209 1.19782L11.1972 1.57501L10.8201 6.36017C10.794 6.69051 11.0407 6.97942 11.3711 7.00546C11.7014 7.0315 11.9903 6.78481 12.0164 6.45446L12.4407 1.07116ZM0.599609 10.624L0.989223 11.0803L12.2322 1.4803L11.8425 1.02401L11.4529 0.56772L0.209996 10.1677L0.599609 10.624Z" fill="black" />
-                                </svg></span><span className='text-md text-white text-16'>Start Now</span></button>
+                                </svg></span><span className='text-md text-white text-16'>Get Your Plans</span></button>
                             </div>
-                        </div>
-                    </div>
-                    <div className="plan-item bg-light-grey">
-                        <div className="plan-title flex">
-                            <h3 className="h4 text-sb">Enterprise</h3>
-                            <div className="p-price flex"><h4 className="text-bd text-black">$299</h4><span className="text-18 text-black"> per user/month</span></div>
-                        </div>
-                        <p className="h6 text-black">A streamlined plan with all the essential tools for efficient case trials and management.</p>
-                        <div className="plan-list">
+                        {/* <div className="plan-list">
                             <h5 className="text-md text-black">Includes</h5>
                             <ul>
                                 <li className="plan-list-text flex"><svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.2454 11.066L0 5.82055L1.31135 4.5092L5.2454 8.44325L13.6887 0L15 1.31135L5.2454 11.066Z" fill="black" /></svg><p className="text-18 text-black">Everything in Standard</p></li>
                                 <li className="plan-list-text flex"><svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.2454 11.066L0 5.82055L1.31135 4.5092L5.2454 8.44325L13.6887 0L15 1.31135L5.2454 11.066Z" fill="black" /></svg><p className="text-18 text-black">Advanced Signal Analytics & Risk Scoring</p></li>
                                 <li className="plan-list-text flex"><svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.2454 11.066L0 5.82055L1.31135 4.5092L5.2454 8.44325L13.6887 0L15 1.31135L5.2454 11.066Z" fill="black" /></svg><p className="text-18 text-black">Automated Aggregate Reporting</p></li>
                                 <li className="plan-list-text flex"><svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.2454 11.066L0 5.82055L1.31135 4.5092L5.2454 8.44325L13.6887 0L15 1.31135L5.2454 11.066Z" fill="black" /></svg><p className="text-18 text-black">Enterprise‑Grade Quality Management</p></li>
-                            </ul>
-                            <div className="plan-btn">
-                                <button className='main-btn flex'><span className='btn-icon centered-block'><svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M0.209996 10.1677C-0.0420067 10.3829 -0.0718591 10.7616 0.143319 11.0136C0.358496 11.2656 0.73722 11.2955 0.989223 11.0803L0.599609 10.624L0.209996 10.1677ZM12.4407 1.07116C12.4667 0.740812 12.22 0.451905 11.8897 0.425866L6.50638 0.00153416C6.17604 -0.0245046 5.88713 0.222185 5.86109 0.552531C5.83505 0.882877 6.08174 1.17178 6.41209 1.19782L11.1972 1.57501L10.8201 6.36017C10.794 6.69051 11.0407 6.97942 11.3711 7.00546C11.7014 7.0315 11.9903 6.78481 12.0164 6.45446L12.4407 1.07116ZM0.599609 10.624L0.989223 11.0803L12.2322 1.4803L11.8425 1.02401L11.4529 0.56772L0.209996 10.1677L0.599609 10.624Z" fill="white" />
-                                </svg></span><span className='text-md text-black text-16'>Start Now</span></button>
-                            </div>
-                        </div>
+                            </ul>                            
+                        </div> */}
                     </div>
                 </div>
             </div>
