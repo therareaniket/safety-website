47e4cfb44f4105033ad90b9edf87651cfbe14d50
therareaniket <aniketbhatti.rarepixelsdesign@gmail.com>
2026-02-01 13:31:12 +0530

    chore 🧹: Updated the layout.tsx to add updated CSS of dev-A

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "@/assets/css/shalaka/custom.css"
 import "@/assets/css/shalaka/responsive.css"
+import "@/assets/css/aniket/custom.css"
+import "@/assets/css/aniket/responsive.css"
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
