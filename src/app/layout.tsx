dcd8decd677fa100b7bb04a29a4dfcfc6908336e
therareaniket <aniketbhatti.rarepixelsdesign@gmail.com>
2026-02-06 12:42:13 +0530

    fix 🐛: Final Container Size change based on Academy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,16 +8,13 @@ import "@/assets/css/aniket/custom.css"
 import "@/assets/css/aniket/responsive.css"
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 export const metadata: Metadata = {
 	title: "DhatuSafety | Central Hub for Patient Safety Intelligence",
 	description: "Empowering the Future of Patient Safety",
 };
 
-export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) 
+export default function RootLayout ({ children, }: Readonly<{ children: React.ReactNode; }>) 
 {
   return (
     <html lang="en">
